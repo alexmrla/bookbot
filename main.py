2a8e07b157cf825f52fda88e6c5a2ead39d346f3
alexmrla <38337488+alexmrla@users.noreply.github.com>
2025-12-30 14:44:13 +0200

    Added sysargs

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from stats import get_num_words, get_char_count, sort_chars
+import sys
 
-def get_book_text():
-    with open('./books/frankenstein.txt') as f:
-        convert_to_string = f.read()
-    return convert_to_string
+
+def get_book_text(input_path):
+    with open(input_path) as f:
+        return f.read()
 
 def main():
-    string = get_book_text()
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    input_path = sys.argv[1]
+    string = get_book_text(input_path)
     total_words = get_num_words(string)
     count_chars = get_char_count(string)
     char_sort = sort_chars(count_chars)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {input_path}...")
     print("----------- Word Count ----------")
     print(f"Found {total_words} total words")
     print("--------- Character Count -------")
